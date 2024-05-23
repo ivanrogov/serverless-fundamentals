a3c2c8ed447b6252f4cbfc912bdb26f9a8786d46
Ivan_Rogov <Ivan_Rogov@epam.com>
2024-05-23 15:23:23 +0300

    Adjust strings

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -7,21 +7,21 @@ exports.handler = async (event) => {
     if (path === '/hello' && method === 'GET'){
       responseJson = {
           statusCode: 200,
-          message: JSON.stringify('Hello from Lambda!'),
+          message: JSON.parse(JSON.stringify('Hello from Lambda!')),
       };
       response = {
           statusCode: 200,
-          body: JSON.stringify(responseJson),
+          body: JSON.parse(JSON.stringify(responseJson)),
       };
     } else {
       var s = `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`;
       responseJson = {
           statusCode: 400,
-          message: JSON.stringify(s),
+          message: JSON.parse(JSON.stringify(s)),
       };
       response = {
           statusCode: 400,
-          body: JSON.stringify(responseJson),
+          body: JSON.parse(JSON.stringify(responseJson)),
       };
     }
     console.log(response);
